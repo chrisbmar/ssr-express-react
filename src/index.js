01e33305299cdb911984e29918d427f656361c56
Chris Martin <c.martin@stuart.com>
2020-07-28 13:01:21 +0100

    boilerplate setup and server side webpack

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const React = require("react");
+const { render } = require("react-dom");
+const renderToString = require("react-dom/server").renderToString;
+const Home = require("./client/components/Home").default;
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    const content = renderToString(<Home />);
+
+    res.send(content);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
---
+++ webpack.server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    // Inform webpack we're building a bundle for Node.js rather than the browser (Default)
+    target: "node",
+    // Tell webpack the root file of our server app
+    entry: "./src/index.js",
+    // Tell webpack where to put the output file that is generated
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build")
+    },
+    // Tell webpack to run babel on every file it runs through
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        "react",
+                        "stage-0",
+                        ["env", { targets: { browsers: ["last 2 versions"]}}]
+                    ]
+                }
+            }
+        ]
+    }
+}
