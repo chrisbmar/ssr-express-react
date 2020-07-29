6d7054021cd626dc9cf6c75241fbb5c36ccdf33d
Chris Martin <c.martin@stuart.com>
2020-07-29 11:14:48 +0100

    create client bundle

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-    return <div>I'm the new home component!</div>
+    return (
+        <div>
+            <div>I'm the new home component!</div>
+            <button onClick={() => console.log("Hi there!")}>Click here!</button>
+        </div>
+    )
 };
 
 export default Home;
