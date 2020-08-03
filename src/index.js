8954e777cdb447055bc59045e83fff6508b8abbe
Chris Martin <c.martin@stuart.com>
2020-08-03 20:41:33 +0200

    add redux support in ssr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 const PORT = 3000;
@@ -7,8 +8,11 @@ const PORT = 3000;
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
+  const store = createStore();
+  // logic to intialise and load data into the store
+
   // renderer is a ssr helper function
-  res.send(renderer(req));
+  res.send(renderer(req, store));
 });
 
 app.listen(PORT, () => {
